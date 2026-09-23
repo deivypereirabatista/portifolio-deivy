c49254cecf63dae7af9b224742d980850b391ca8
Deivy Batista <deivy.batista.ba@gmail.com>
2026-09-23 11:27:27 -0300

    feat: cria primeira seção do portfólio

---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+function Hero() {
+    return(
+        <section>
+        <p>Olá, eu sou</p>
+
+        <h1>Deivy Batista</h1>
+
+        <h2>Desenvolvedor Web</h2>
+
+        <p>
+          Desenvolvo aplicações web com foco em JavaScript, React e Python.
+        </p>
+
+        <button>Ver meus projetos</button>
+      </section>
+    )
+}
+
+export default Hero
